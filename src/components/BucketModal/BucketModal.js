03694d9d48c932b94d24ae72ef7f7b948614bfc7
kudybyn <kudybynwork33@gmail.com>
2024-07-17 15:56:49 +0200

    change close icon

--- src/components/BucketModal/BucketModal.js
+++ src/components/BucketModal/BucketModal.js
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import Trash from "../../assets/trash.svg"
 import { useDispatch } from 'react-redux'
 import { remove } from '../../redux/slices/BucketSlice'
+import Close from "../../assets/close.svg";
 
 export default function BucketModal({ onClose, data }) {
     const {t} = useTranslation();
@@ -26,7 +27,7 @@ const dispatch = useDispatch()
         <div className="fixed w-full h-full bg-[rgba(0,0,0,0.3)] flex items-center justify-center z-[1000]" onClick={onClose}>
             <div className="lg:rounded-xl bg-white shadow-lg px-8 py-4 w-[100vw] h-[100vh] lg:w-[60vw] lg:h-[75vh] max-h-[100vh] lg:max-h-[75vh]" onClick={handleModalClick}>
                 <div className="w-full flex justify-end">
-                    <button className="font-bold" onClick={onClose}>X</button>
+                    <button className="font-bold" onClick={onClose}><img src={Close} className="w-[30px] h-[30px]" alt="close"/></button>
                 </div>
                 <div className="w-full flex flex-col gap-8  items-center mt-[15px] h-full max-h-[75vh] lg:max-h-[55vh]">
                     <span className="font-bold text-[27px]">{t('bucket.yourBucket')}</span>
